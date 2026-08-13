8b34fe4a74d30e0b243a1514d63b665edc74d393
mohmdrezaei <mhwmdrz9@gmail.com>
2026-08-13 12:43:09 +0330

    add bundleAnalyzer package and optimize docker

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+import withBundleAnalyzer from "@next/bundle-analyzer";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  poweredByHeader: false,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+})(nextConfig);
